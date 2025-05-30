f7265f0c563600c5b30a3ab5656898d0a7d11ef8
umer134 <71734364+umer134@users.noreply.github.com>
2025-05-30 21:07:14 +0300

    21:07

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    rules: {
+      "jsx-a11y/alt-text": "off",
+    },
+  },
 ];
+
 
 export default eslintConfig;
